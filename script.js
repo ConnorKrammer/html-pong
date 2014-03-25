91bbc374ea5908819117a6a0cb2e2bd0b56a7157
Connor Krammer <connork99@gmail.com>
2014-03-24 23:28:15 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// This fetches that div element that we set the id attribute on.
+var div = document.getElementById('content');
+
+// The element is an object, so we can change its properties.
+// Here we set the background color to red. This is equivalent to setting the property in CSS.
+div.style.backgroundColor = "red";
